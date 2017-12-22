8237574579f0121d0104f2d6683ab5d9f377157f
sterie <sterie@acerX3950>
2017-12-22 22:37:28 +0300

    edit ids. Add generateNote, generateId, generateDescription

--- src/main/java/ru/sfedu/organizer/utils/MyGenerator.java
+++ src/main/java/ru/sfedu/organizer/utils/MyGenerator.java
@@ -28,28 +28,28 @@ public class MyGenerator {
 //        
 //    }
     
-    public static Long generateDateLong() throws ParseException, Exception{
-        return generateDateLong("1800-01-01", "2017-01-01");
-    }
-    
-    public static Long generateDateLong(String begin, String end) throws ParseException, Exception{
-        SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd");
-        long beginLong = ft.parse(begin).getTime();      
-        long endLong = ft.parse(end).getTime();
-        if (beginLong > endLong) throw new Exception("begin date > end date"); 
-        return generateDateLong(begin, end);      
-    }
-    
-    public static Long generateDateLong(Date begin, Date end){
-        return generateDateLong(begin.getTime(), end.getTime());        
-    }
-    
-    public static Long generateDateLong(long begin, long end){
-        if (begin > end) return (long)0;
-        long dif = end - begin;
-        long date = Math.round(Math.random() * dif + begin);        
-        return date;
-    }
+//    public static Long generateDateLong() throws ParseException, Exception{
+//        return generateDateLong("1800-01-01", "2017-01-01");
+//    }
+//    
+//    public static Long generateDateLong(String begin, String end) throws ParseException, Exception{
+//        SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd");
+//        long beginLong = ft.parse(begin).getTime();      
+//        long endLong = ft.parse(end).getTime();
+//        if (beginLong > endLong) throw new Exception("begin date > end date"); 
+//        return generateDateLong(begin, end);      
+//    }
+//    
+//    public static Long generateDateLong(Date begin, Date end){
+//        return generateDateLong(begin.getTime(), end.getTime());        
+//    }
+//    
+//    public static Long generateDateLong(long begin, long end){
+//        if (begin > end) return (long)0;
+//        long dif = end - begin;
+//        long date = Math.round(Math.random() * dif + begin);        
+//        return date;
+//    }
 
     public static Types generateType(){
         int num = 0;
